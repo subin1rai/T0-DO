03ade1a415f4532ab3bcf1df7c77e7dee66aa908
MR. Subi <bantawasubin@gmail.com>
2023-08-18 00:10:01 +0545

    Delete API

--- lib/Screens/todo_list.dart
+++ lib/Screens/todo_list.dart
@@ -20,6 +20,7 @@ class _ToDoListPageState extends State<ToDoListPage> {
     super.initState();
     fetchTodo();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,48 +30,47 @@ class _ToDoListPageState extends State<ToDoListPage> {
       ),
       body: Visibility(
         visible: isloading,
-         child: Center(child: CircularProgressIndicator()),
+        child: Center(child: CircularProgressIndicator()),
         replacement: RefreshIndicator(
           onRefresh: fetchTodo,
           child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index){
-              final item = items[index];
-            return ListTile(
-              leading: CircleAvatar(child: Text('${index+1}')),
-                title: Text(item['title']),
-                subtitle: Text(item['description']),
-                trailing: PopupMenuButton(
-                  onSelected: (value){
-                    if(value == 'edit'){
-                      //open edit
-                    }else if(value =='delete'){
-
-                      //delete and remove the item
-                       
-                    } 
-                  },
-                  
-                  itemBuilder: (context){
-                   
-                    return [
-                      PopupMenuItem(child: Text('Edit'),
-                      value: 'edit',
-                      ),
-
-                      PopupMenuItem(child: Text('Delete'),
-                      value: 'delete',
-                      )
-                    ]; 
-                  },
-                ),
-            );
-          }),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                final id = item['_id'];
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${index + 1}')),
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                  trailing: PopupMenuButton(
+                    onSelected: (value) {
+                      if (value == 'edit') {
+                        //open edit
+                      } else if (value == 'delete') {
+                        //delete and remove the item
+                        deleteById(id);
+                      }
+                    },
+                    itemBuilder: (context) {
+                      return [
+                        PopupMenuItem(
+                          child: Text('Edit'),
+                          value: 'edit',
+                        ),
+                        PopupMenuItem(
+                          child: Text('Delete'),
+                          value: 'delete',
+                        )
+                      ];
+                    },
+                  ),
+                );
+              }),
         ),
       ),
-      floatingActionButton:
-          FloatingActionButton.extended(onPressed: navigateToAddPage,
-           label: const Text("Add")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add,size: 35,),onPressed: navigateToAddPage,
+      ),
     );
   }
 
@@ -82,24 +82,42 @@ class _ToDoListPageState extends State<ToDoListPage> {
     Navigator.push(context, route);
   }
 
+  Future <void> deleteById(String id) async{
+    //delete the item 
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+
+    final response = await http.delete(uri);
+    if(response.statusCode ==200){
+
+    //remove the item
+    final filtered =items.where((element) => element['_id'] !=id).toList();
+      setState(() {
+        items = filtered;
+      });
+    }
+    else{
+      //show error
+    }
+  }
+
   //api get all
-  Future <void> fetchTodo()async{
-   
+  Future<void> fetchTodo() async {
     final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
 
     //display data
-    if(response.statusCode == 200){
-      final json = jsonDecode(response.body)as Map;
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
       final result = json['items'] as List;
       setState(() {
         items = result;
       });
-     
-    setState(() {
-      isloading = false;
-    });
+
+      setState(() {
+        isloading = false;
+      });
+    }
   }
-}
 }
